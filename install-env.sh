1069b606f7f346fbbb83af8c06e913fd66c71ae1
Nandini Sridharan <nandini9590@gmail.com>
2015-10-30 00:10:38 -0500

    changing the paths

--- install-env.sh
+++ install-env.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 sudo apt-get update && sudo apt-get install -y apache2 git php5 php5-mysql php5-curl mysql-client curl
 cd /var/www/html
-sudo git clone https://github.com/Nandini-ITM544/MP1_Application-Setup-Final.git
-sudo mv -v MP1_Application-Setup-Final/* /var/www/html
-sudo rm -rf /var/www/html/MP1_Application-Setup-Final
+sudo git clone https://github.com/Nandini-ITM544/MP1_Application-Setup_Final.git
+sudo mv -v MP1_Application-Setup_Final/* /var/www/html
+sudo rm -rf /var/www/html/MP1_Application-Setup_Final
 cd /home/controller
 sudo curl -sS https://getcomposer.org/installer | php
 sudo php composer.phar require aws/aws-sdk-php
